6b00f5527bcac3629758fec897a968a35da3be95
Cole Falxa-Sturken <colefalxa@gmail.com>
2025-11-24 23:53:23 -0800

    Move components to their own file and add component architecture with LightComponent, CameraComponent, and ScriptComponent

---
+++ golf-bowling/src/components.ts
@@ -0,0 +1,152 @@
+import * as Globals from './globals.ts';
+import { ColliderDesc, RigidBodyDesc, RigidBodyType, type Collider, type RigidBody } from "@dimforge/rapier3d-compat";
+import type { Component, GameObject, Vector3 } from "./types";
+import { Camera, Light, Material, Mesh, PointLight } from "three";
+
+class BaseComponent implements Component {
+    dependencies: (new (gameObject: GameObject) => Component)[] = [];
+    private _gameObject: GameObject;
+    constructor(gameObject: GameObject) {
+        this._gameObject = gameObject;
+    }
+    get gameObject(): GameObject {
+        return this._gameObject;
+    }
+}
+
+export class TransformComponent extends BaseComponent {
+    public position: Vector3;
+    public rotation: Vector3;
+    public scale: Vector3;
+
+    constructor(gameObject: GameObject) {
+        super(gameObject);
+        this.position = { x: 0, y: 0, z: 0 };
+        this.rotation = { x: 0, y: 0, z: 0 };
+        this.scale = { x: 1, y: 1, z: 1 };
+    }
+}
+
+export class RigidbodyComponent extends BaseComponent {
+    dependencies = [TransformComponent];
+    mass: number = 1;
+    velocity: Vector3 = { x: 0, y: 0, z: 0 };
+    private _rigidbodyInstance: RigidBody;
+    private _collider: Collider;
+
+    get rigidbodyType(): "dynamic" | "static" | "kinematic" {
+        switch (this._rigidbodyInstance.bodyType()) {
+            case RigidBodyType.Dynamic:
+                return 'dynamic';
+            case RigidBodyType.Fixed:
+                return 'static';
+            case RigidBodyType.KinematicPositionBased:
+                return 'kinematic';
+            default:
+                throw new Error("Unknown rigidbody type");
+        }
+    }
+
+    set rigidbodyType(value: "dynamic" | "static" | "kinematic") {
+        let bodyType: RigidBodyType;
+        switch (value) {
+            case 'dynamic':
+                bodyType = RigidBodyType.Dynamic;
+                break;
+            case 'static':
+                bodyType = RigidBodyType.Fixed;
+                break;
+            case 'kinematic':
+                bodyType = RigidBodyType.KinematicPositionBased;
+                break;
+        }
+        this._rigidbodyInstance.setBodyType(bodyType, true);
+    }
+
+    constructor(gameObject: GameObject) {
+        super(gameObject);
+        const desc = new RigidBodyDesc(RigidBodyType.Dynamic);
+        this._rigidbodyInstance = Globals.world.createRigidBody(desc);
+        this._collider = Globals.world.createCollider(ColliderDesc.ball(5), this._rigidbodyInstance);
+    }
+
+    dispose() {
+        Globals.world.removeRigidBody(this._rigidbodyInstance);
+    }
+
+    physicsUpdate(_deltaTime: number): void {
+        const translation = this._rigidbodyInstance.translation();
+        this.gameObject.getComponent(TransformComponent)!.position = { ...translation };
+    }
+
+    addCollider(colliderDesc: ColliderDesc) {
+        this._collider = Globals.world.createCollider(colliderDesc, this._rigidbodyInstance);
+    }
+}
+
+export class MeshComponent extends BaseComponent {
+    dependencies = [TransformComponent];
+    public mesh: Mesh;
+
+    public get material(): Material {
+        return this.mesh.material as Material;
+    }
+
+    constructor(gameObject: GameObject) {
+        super(gameObject);
+        this.mesh = new Mesh();
+    }
+
+    renderUpdate(_deltaTime: number): void {
+        const transform = this.gameObject.getComponent(TransformComponent)!;
+        this.mesh.position.set(transform.position.x, transform.position.y, transform.position.z);
+        this.mesh.rotation.set(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+        this.mesh.scale.set(transform.scale.x, transform.scale.y, transform.scale.z);
+    }
+}
+
+export class LightComponent extends BaseComponent {
+    dependencies = [TransformComponent];
+    public light: Light;
+    constructor(gameObject: GameObject) {
+        super(gameObject);
+        this.light = new PointLight();
+    }
+
+    renderUpdate(_deltaTime: number): void {
+        const transform = this.gameObject.getComponent(TransformComponent)!;
+        this.light.position.set(transform.position.x, transform.position.y, transform.position.z);
+        this.light.rotation.set(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
+}
+
+export class CameraComponent extends BaseComponent {
+    dependencies = [TransformComponent];
+    public camera: Camera;
+    constructor(gameObject: GameObject) {
+        super(gameObject);
+        this.camera = new Camera();
+    }
+}
+
+export class ScriptComponent extends BaseComponent {
+    public onCreate?(): void;
+    public onStart?(): void;
+    public onUpdate?(_deltaTime: number): void;
+    public onPhysicsUpdate?(_deltaTime: number): void;
+    private hasStarted: boolean = false;
+
+    create(): void {
+        this.onCreate?.();
+    }
+    renderUpdate(_deltaTime: number): void {
+        if (!this.hasStarted) {
+            this.onStart?.();
+            this.hasStarted = true;
+        }
+        this.onUpdate?.(_deltaTime);
+    }
+    physicsUpdate(_deltaTime: number): void {
+        this.onPhysicsUpdate?.(_deltaTime);
+    }
+}
--- golf-bowling/src/types.ts
+++ golf-bowling/src/types.ts
@@ -1,7 +1,3 @@
-import { Collider, ColliderDesc, RigidBody, RigidBodyDesc, RigidBodyType} from "@dimforge/rapier3d-compat";
-import * as Globals from './globals.ts';
-import { Material, Mesh } from "three";
-
 export interface Vector3 {
     x: number;
     y: number;
@@ -10,118 +6,20 @@ export interface Vector3 {
 
 export interface GameObject {
     id: string;
-    transform: TransformComponent | null;
-    components: Component[];
+    set active(value: boolean);
+    get active(): boolean;
     addComponent<T extends Component>(componentType: new (gameObject: GameObject) => T): T;
     getComponent<T extends Component>(componentType: new (gameObject: GameObject) => T): T | null;
     getComponents<T extends Component>(componentType: new (gameObject: GameObject) => T): T[];
     removeComponent<T extends Component>(componentType: new (gameObject: GameObject) => T): void;
-    destroy(): void;
+    dispose(): void;
 }
-
-
 
 export interface Component {
     dependencies: (new (gameObject: GameObject) => Component)[];
     get gameObject(): GameObject;
-    update?(deltaTime: number): void;
+    create?(): void;
+    renderUpdate?(deltaTime: number): void;
+    physicsUpdate?(deltaTime: number): void;
     dispose?(): void;
-}
-
-class BaseComponent implements Component {
-    dependencies: (new (gameObject: GameObject) => Component)[] = [];
-    private _gameObject: GameObject;
-    constructor(gameObject: GameObject) {
-        this._gameObject = gameObject;
-    }
-    get gameObject(): GameObject {
-        return this._gameObject;
-    }
-}
-
-export class TransformComponent extends BaseComponent {
-    public position: Vector3;
-    public rotation: Vector3;
-    public scale: Vector3;
-
-    constructor(gameObject: GameObject) {
-        super(gameObject);
-        this.position = { x: 0, y: 0, z: 0 };
-        this.rotation = { x: 0, y: 0, z: 0 };
-        this.scale = { x: 1, y: 1, z: 1 };
-    }
-}
-
-export class RigidbodyComponent extends BaseComponent {
-    dependencies = [TransformComponent];
-    mass: number = 1;
-    velocity: Vector3 = { x: 0, y: 0, z: 0 };
-    private _rigidbodyInstance: RigidBody;
-    private _collider: Collider;
-
-    get rigidbodyType(): "dynamic" | "static" | "kinematic" {
-        switch (this._rigidbodyInstance.bodyType()) {
-            case RigidBodyType.Dynamic:
-                return 'dynamic';
-            case RigidBodyType.Fixed:
-                return 'static';
-            case RigidBodyType.KinematicPositionBased:
-                return 'kinematic';
-            default:
-                throw new Error("Unknown rigidbody type");
-        }
-    }
-
-    set rigidbodyType(value: "dynamic" | "static" | "kinematic") {
-        let bodyType: RigidBodyType;
-        switch (value) {
-            case 'dynamic':
-                bodyType = RigidBodyType.Dynamic;
-                break;
-            case 'static':
-                bodyType = RigidBodyType.Fixed;
-                break;
-            case 'kinematic':
-                bodyType = RigidBodyType.KinematicPositionBased;
-                break;
-        }
-        this._rigidbodyInstance.setBodyType(bodyType, true);
-    }
-
-    constructor(gameObject: GameObject) {
-        super(gameObject);
-        const desc = new RigidBodyDesc(RigidBodyType.Dynamic);
-        this._rigidbodyInstance = Globals.world.createRigidBody(desc);
-        this._collider = Globals.world.createCollider(ColliderDesc.ball(5), this._rigidbodyInstance);
-    }
-
-    dispose() {
-        Globals.world.removeRigidBody(this._rigidbodyInstance);
-    }
-
-    update(_deltaTime: number): void {
-        const translation = this._rigidbodyInstance.translation();
-        this.gameObject.getComponent(TransformComponent)!.position = { ...translation };
-    }
-}
-
-export class MeshComponent extends BaseComponent {
-    dependencies = [TransformComponent];
-    public mesh: Mesh;
-
-    public get material(): Material {
-        return this.mesh.material as Material;
-    }
-
-    constructor(gameObject: GameObject) {
-        super(gameObject);
-        this.mesh = new Mesh();
-    }
-
-    update(_deltaTime: number): void {
-        const transform = this.gameObject.getComponent(TransformComponent)!;
-        this.mesh.position.set(transform.position.x, transform.position.y, transform.position.z);
-        this.mesh.rotation.set(transform.rotation.x, transform.rotation.y, transform.rotation.z);
-        this.mesh.scale.set(transform.scale.x, transform.scale.y, transform.scale.z);
-    }
 }
