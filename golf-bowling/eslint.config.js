4c3c189443568b4c7d3dc1cbe25b8c9219ec694f
bfereydouni023 <bfereydo@gmail.com>
2025-11-28 21:49:11 -0800

    Got rid of redundant code and old comments.

--- golf-bowling/eslint.config.js
+++ golf-bowling/eslint.config.js
@@ -1,30 +1,10 @@
 import jseslint from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
-import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
-
   jseslint.configs.recommended,   // ESLint recommended base rules 
   tseslint.configs.recommended, // TypeScript recommended configs from @typescript-eslint
   eslintConfigPrettier, //Makes sure Prettier and ESLint run smoothly together
-
-  { //making sure that the intended files are viewed by the linter
-    files: ['src/**/*.ts'],
-
-    //additional rules aside from those provided in the recommended configs
-    rules: {
-      // cleanup for unused variables
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  },
 );
