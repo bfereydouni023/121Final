ef89fa7e457a481603b4abfe5acdb3b624fc7e14
Cole Falxa-Sturken <colefalxa@gmail.com>
2025-12-01 13:28:53 -0800

    Swap to using Prettier instead of Prettier Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,19 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
-  jseslint.configs.recommended, // ESLint recommended base rules
-  tseslint.configs.recommended, // TypeScript recommended configs from @typescript-eslint
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
+    jseslint.configs.recommended, // ESLint recommended base rules
+    tseslint.configs.recommended, // TypeScript recommended configs from @typescript-eslint
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
         },
-      ],
     },
-  },
-  eslintConfigPrettier, //Makes sure Prettier and ESLint run smoothly together
+    eslintConfigPrettier, //Makes sure Prettier and ESLint run smoothly together
 );
